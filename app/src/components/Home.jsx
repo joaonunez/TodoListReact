2b66084a4c4315d62790901c863694b7b04db32f
joaonunez <joaovaldiglesias@gmail.com>
2024-07-12 19:45:51 -0400

    created home.jsx

---
+++ app/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+export const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <ul>
+            <li>Make the bed</li>
+            <li>Make the door</li>
+            <li>Make the toy</li>
+            <li>Walk the dog</li>
+        </ul>
+      </div>
+    </>
+  );
+};
