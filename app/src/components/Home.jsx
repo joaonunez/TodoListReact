98ebc212f15619ce937fefaaf089ac9f6e113d78
joaonunez <joaovaldiglesias@gmail.com>
2024-07-12 21:14:49 -0400

    created home.jsx

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -3,12 +3,15 @@ export const Home = () => {
   return (
     <>
       <div className="container">
+        <h1>Gestión De Tareas</h1>
         <ul>
+            <li><input type="text" placeholder="Ingresa Una Nueva Tarea"></input></li>
             <li>Make the bed</li>
             <li>Make the door</li>
             <li>Make the toy</li>
             <li>Walk the dog</li>
         </ul>
+        <div>23 Tasks</div>
       </div>
     </>
   );
