94c0d057cea25a1e3a6451e422fa8f40abd7c020
joaonunez <joaovaldiglesias@gmail.com>
2024-07-15 20:37:00 -0400

    avance

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
 export const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
       <div className="container">
         <h1>Gestión De Tareas</h1>
         <ul>
-            <li><input type="text" placeholder="Ingresa Una Nueva Tarea"></input></li>
-            <li>Make the bed</li>
-            <li>Make the door</li>
-            <li>Make the toy</li>
-            <li>Walk the dog</li>
+          <li>
+            <input
+              type="text"
+              placeholder="Ingresa Una Nueva Tarea"
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+              onKeyPress={(e) => {
+                console.log(e.key);
+                if (e.key === "Enter") {
+                  setTodos(todos.concat(inputValue));
+                  setInputValue("");
+                }
+              }}
+            ></input>
+          </li>
+          {todos.map((t) => (
+            <li>
+              {t} <FontAwesomeIcon icon={faTrash} />
+            </li>
+          ))}
+          
         </ul>
         <div>23 Tasks</div>
       </div>
